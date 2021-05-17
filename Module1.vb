0186076c5e282fc678d6661b266b6b17fbd047d7
Johao Lopez <johao.lopz.m@gmail.com>
2021-05-16 22:38:36 -0500

    Probando var, const y console .Write() .readLine()

---
+++ Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As Integer
+        numero = 12
+        'otra forma es Dim numero As Integer = 12
+        Const PI As Double = 3.1416
+
+        Console.Write("Ingrese un radio: ")
+        Dim radio As Double = Console.ReadLine()
+        Dim area As Double = PI * radio * radio
+
+        Console.WriteLine("El area es: " & area)
+        'La linea acotinuacion es para pausar hasta presionar una tecla
+        'Console.ReadKey(True)"
+
+    End Sub
+
+End Module
