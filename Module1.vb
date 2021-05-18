457a761a028cab560cd84b760e4ac242d543929c
Johao Lopez <johao.lopz.m@gmail.com>
2021-05-17 20:46:19 -0500

    Recorriendo matriz con bucles anidados - Uso de GetUpperBound

--- Module1.vb
+++ Module1.vb
@@ -1,19 +1,28 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Bienvenidos al Ejercicio BUCLE WHILE")
+        Console.WriteLine("Bienvenidos al Ejercicio BUCLE ANIDADO Y METODO GetUpperBound")
         Console.WriteLine("")
-        Console.Write("Ingrese un numero entero (ej. 26754): ")
-        Dim numero As Integer = Console.ReadLine()
-        Console.WriteLine("")
-        Dim sumaDigitos As Integer = 0
+        Console.WriteLine("La siguiente matriz es 2x3")
+        Dim matriz(2, 3) As Integer
 
-        While numero > 0
-            sumaDigitos += numero Mod 10
-            numero = numero \ 10
-            'Én la división entera se puede usar   numero = Math.Truncate(numero / 10)
-        End While
-        Console.WriteLine("La suma de los digitos es: " & sumaDigitos)
+        matriz(0, 0) = 49
+        matriz(0, 1) = 57
+        matriz(0, 2) = 28
+        matriz(1, 0) = 16
+        matriz(1, 1) = 3
+        matriz(1, 2) = 89
+
+        'GetUpperBound capta el índice del último elemento de la fila de un array
+        For i = 0 To matriz.GetUpperBound(0) - 1 Step 1
+
+            For j = 0 To matriz.GetUpperBound(1) - 1 Step 1
+
+                Console.Write(matriz(i, j) & " ")
+
+            Next
+            Console.WriteLine()
+        Next
 
     End Sub
 
